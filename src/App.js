b6b0cfc61cfd9bb1b9441f9a9264a2ace24ec791
jrestrepo922 <tinto922@gmail.com>
2021-06-20 09:54:11 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Home from './Home/Home'
+import Header from './Header/Header'
+
+function App() {
+  return (
+    //BEM
+    <div className="App">
+
+      <Header/>
+      <Home/>
+          {/*header */}
+          {/*Banner */}
+            {/*search */}
+          {/*cards */}
+          {/*footer*/}
+
+      {/*SearchPage */}  
+          {/*header */}
+          {/*....*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Search/Search.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+//DATE PICKER COMPONENT 
+function Search() {
+    return (
+        <div className="search">
+            
+        </div>
+    )
+}
+
+export default Search
