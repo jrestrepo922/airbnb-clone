169cd2c95035c26e7ace50332623ffc1ffba7944
jrestrepo922 <tinto922@gmail.com>
2021-07-21 08:49:15 -0400

    adding mobile design for the stay details page

---
+++ src/CarouselLarge/CarouselLarge.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import "./CarouselLarge.css";
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import { useHistory } from 'react-router-dom';
+
+function CarouselLarge({stayImages, superHost, id}) {
+
+
+    const history = useHistory(); 
+    let [currImg, setCurrentImg] = useState(0); 
+
+    function handleClickBack(event) {
+        if(currImg === 0){
+
+        } else {
+            setCurrentImg(--currImg);
+        }
+        
+    }
+
+
+    function handleClickNext(event){
+        if(currImg > (stayImages.length - 2)){
+            setCurrentImg(0); 
+        } else {
+            setCurrentImg(++currImg);
+        }
+    }   
+
+    function handleRedirect(event){
+        if(event.target.id !== "back" && event.target.id !== "next"){
+            history.push(`/search/${id}`)
+        }
+    }
+
+    return (
+        <div className="carouselLarge">
+            <div 
+                className="carouselLarge__inner"
+                style={{backgroundImage: `url(http://localhost:3000/${stayImages[currImg]})`}}
+                onClick={handleRedirect}
+            >
+                
+                <div className="left" onClick={handleClickBack}><NavigateBeforeIcon id="back"/></div> 
+                <div className="right" onClick={handleClickNext}><NavigateNextIcon id="next"/></div>
+            </div>
+        </div>
+    )
+}
+
+export default CarouselLarge
--- src/StayDetailsMin/StayDetailsMin.js
+++ src/StayDetailsMin/StayDetailsMin.js
@@ -10,7 +10,7 @@ import { editHostIsLiked } from '../SearchPage/searchPageSlice';
 import { selectSearch } from "../Search/searchSlice"; 
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import WhatshotIcon from '@material-ui/icons/Whatshot';
-import Carousel  from '../Carousel/Carousel';
+import CarouselLarge  from '../CarouselLarge/CarouselLarge';
 
 
 
@@ -32,9 +32,9 @@ function StayDetailsMin(props) {
     }
 
     const isSuperHost = (
-        <div className="stay__superHostContainer">
-            <div className="stay__superHostInner">
-                <WhatshotIcon className="stay__hotIcon"/>
+        <div className="stayMin__superHostContainer">
+            <div className="stayMin__superHostInner">
+                <WhatshotIcon className="stayMin__hotIcon"/>
                 <h3>{host} is a Superhost </h3>
             </div>
             <p>Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests</p>
@@ -42,9 +42,9 @@ function StayDetailsMin(props) {
     )
 
     const isSuperHostHeader = (
-        <div className="stay__superHostHeaderContainer">
-            <WhatshotIcon className="stay__hotIconHeader"/>
-            <p > superhost   <span> ·</span> </p>
+        <div className="stayMin__superHostHeaderContainer">
+            <WhatshotIcon className="stayMin__hotIconHeader"/>
+            <p > superhost   </p>
         </div>
 
     )
@@ -55,9 +55,9 @@ function StayDetailsMin(props) {
     useEffect(() => {
         //eslint-disable-next-line 
         if(searchGuest){
-            document.querySelector('.stay__checkOutContainer').style.height = "457px";  
+            document.querySelector('.stayMin__checkOutContainer').style.height = "457px";  
         } else {
-            document.querySelector('.stay__checkOutContainer').style.height = "227px";  
+            document.querySelector('.stayMin__checkOutContainer').style.height = "227px";  
         }
 
         window.scrollTo(0, 0);
@@ -66,25 +66,25 @@ function StayDetailsMin(props) {
 
 
     const total = (
-        <div className="stay__totalContainer">
+        <div className="stayMin__totalContainer">
             <p>You won't be Charge yet</p>
-            <div className="stay__totalPriceContainer">
+            <div className="stayMin__totalPriceContainer">
                <p> ${pricePerDay} X {totalnights} nights</p>
                <p>${pricePerDay * totalnights}</p>
             </div>
-            <div className="stay__cleaningFee">
+            <div className="stayMin__cleaningFee">
                 <p>Cleaning fee</p>
                 <p>$200</p>
             </div>
-            <div className="stay__serviceFee">
+            <div className="stayMin__serviceFee">
                 <p>Service fee</p>
                 <p>$333</p>
             </div>
-            <div className="stay__taxesFee">
+            <div className="stayMin__taxesFee">
                 <p>Taxes and fees</p>
                 <p>$165</p>
             </div>
-            <div className="stay__allFeesContainer">
+            <div className="stayMin__allFeesContainer">
                 <p>Total</p>
                 <p>${698 + (pricePerDay * totalnights)}</p>
             </div>
@@ -92,91 +92,94 @@ function StayDetailsMin(props) {
     )
 
     return (
-        <div className="stay__container">
-            <h2>{title}</h2>
-            <div className="stay__reviewLocationShareLikeContainer">
-                <div className="stay__reviewLocationContainer">
-                    <StarIcon className="stay__star"/>
-                    <p>{stars}   <span className="stay__reviewLocationContainerSpam1"> · </span></p>
-                    <p> {superHost? isSuperHostHeader: ""} </p>
-                    <p><span className="stay__reviewLocationContainerSpam2">{location}</span></p>
-                </div>
-                <div className="stay__shareLikeContainer">
-                    <DeckIcon className="stay__deck"/> 
-                    <p>Share</p>
-                    {liked ? <FavoriteIcon className="stay__liked" onClick={handleClick}/> : <FavoriteBorderIcon className="stay__notLiked" onClick={handleClick}/>}
-                    <p>save</p>
-                </div>
-            </div>
-
-
-
-
-
-            <div className="stay__imgOuterContainer">
-                <Carousel stayImages={stayImages} superHost={superHost} id={id}/>
-            </div>
-
-            <div className="stay_detailInfoContainer">
-                <div className="stay__detailInfo">
-                    <div className="stay__info1">
-                        <div className="stay__infoInner">
-                            <h2>Entire condominium hosted by {host}</h2>
-                            <p>{guests} {guests? "guests": "guest"} · {bedrooms} {bedrooms? "bedrooms": "bedroom"} ·  {beds} {beds? "beds": "bed"} · {baths} {baths? "baths": "bath"} </p>
-                        </div>
-                        <div className="stay__hostImg">
-                            <img src={`../${hostPic}`} alt="host"/>
-                        </div>
-                        
-                    </div>
-                    {superHost? isSuperHost: ""}
-                    <div className="stay__info2">
-                        <ul>
-                            <li>Entire Home to yourself</li>
-                            <li>Commited to Enhanced Clean</li>
-                        </ul>
-                    </div>
-
-                    <div className="stay_info3">
-                        <h2>All about {host}'s place</h2>
-                        <p>{description}</p>
-                    </div>
-                </div>
-                <div className="stay__checkOutContainer">
-                    <div className="stay__priceStars">
-                        <p> <span>$ {pricePerDay}</span>/ night</p>
-                        <div className="stay__reviewLocationContainer">
-                            <StarIcon className="stay__star"/>
-                            <p>{stars}</p>
-                        </div>
-                    </div>
-                    <div className="stay__checkInCheckOutGuestContainer">
-                        <div className="stay__checkInCheckOutContainer">
-                            <h5>CHECK-IN</h5>
-                            <p>{searchGuest ? formatedStarDate  : "Add date" }</p>
-                        </div>
-                        <div className="stay__vl"></div>
-                        <div className="stay__checkInCheckOutContainer">
-                            <h5>CHECK-OUT</h5>
-                            <p>{searchGuest ? formatedEndDate  : "Add date" }</p>
-                        </div>
-                        <div className="stay__guestsContainer">
-                            <h5>guests</h5>
-                            <div className="stay__guestsContainerInnerContainer">
-                                <p> {searchGuest? `${searchGuest} guest`: " 1 guest " } </p>
-                                <ExpandMoreIcon className="stay__expandMoreIcon"/>
-                            </div>
-
-                        </div>
+        <div className="stayMin__container">
+            <CarouselLarge stayImages={stayImages} superHost={superHost} id={id}/>
+            <div className="stayMin__innerContainer">
+                <h2>{title}</h2>
+                <div className="stayMin__reviewLocationShareLikeContainer">
+                    
+                    <div className="stayMin__reviewLocationContainer">
+                        <StarIcon className="stayMin__star"/>
+                        <p>{stars}   <span className="stayMin__reviewLocationContainerSpam1"> · </span></p>
+                        <p> {superHost? isSuperHostHeader: ""} </p>
                     </div>
                     
-                    <button>{searchGuest? "Reserve" : "Check availability"}</button>
-                    {searchGuest? total: ""}
+                    <div className="stayMin__shareLikeContainer">
+                        <DeckIcon className="stayMin__deck"/> 
+                        <p>Share</p>
+                        {liked ? <FavoriteIcon className="stayMin__liked" onClick={handleClick}/> : <FavoriteBorderIcon className="stay__notLiked" onClick={handleClick}/>}
+                        <p>save</p>
+                    </div>
                 </div>
-             </div>
+                <p><span className="stayMin__reviewLocationContainerSpam2">{location}</span></p>
+                
 
 
 
+
+
+                
+                    
+            
+
+                <div className="stayMin__detailInfoContainer">
+                    <div className="stayMin__detailInfo">
+                        <div className="stayMin__info1">
+                            <div className="stayMin__infoInner">
+                                <h2>Entire condominium hosted by {host}</h2>
+                                <p>{guests} {guests? "guests": "guest"} · {bedrooms} {bedrooms? "bedrooms": "bedroom"} ·  {beds} {beds? "beds": "bed"} · {baths} {baths? "baths": "bath"} </p>
+                            </div>
+                            <div className="stayMin__hostImg">
+                                <img src={`../${hostPic}`} alt="host"/>
+                            </div>
+                            
+                        </div>
+                        {superHost? isSuperHost: ""}
+                        <div className="stayMin__info2">
+                            <ul>
+                                <li>Entire Home to yourself</li>
+                                <li>Commited to Enhanced Clean</li>
+                            </ul>
+                        </div>
+
+                        <div className="stayMin__info3">
+                            <h2>All about {host}'s place</h2>
+                            <p>{description}</p>
+                        </div>
+                    </div>
+                    <div className="stayMin__checkOutContainer">
+                        <div className="stayMin__priceStars">
+                            <p> <span>$ {pricePerDay}</span>/ night</p>
+                            <div className="stayMin__reviewLocationContainer">
+                                <StarIcon className="stayMin__star"/>
+                                <p>{stars}</p>
+                            </div>
+                        </div>
+                        <div className="stayMin__checkInCheckOutGuestContainer">
+                            <div className="stayMin__checkInCheckOutContainer">
+                                <h5>CHECK-IN</h5>
+                                <p>{searchGuest ? formatedStarDate  : "Add date" }</p>
+                            </div>
+                            <div className="stayMin__vl"></div>
+                            <div className="stayMin__checkInCheckOutContainer">
+                                <h5>CHECK-OUT</h5>
+                                <p>{searchGuest ? formatedEndDate  : "Add date" }</p>
+                            </div>
+                            <div className="stayMin__guestsContainer">
+                                <h5>guests</h5>
+                                <div className="stayMin__guestsContainerInnerContainer">
+                                    <p> {searchGuest? `${searchGuest} guest`: " 1 guest " } </p>
+                                    <ExpandMoreIcon className="stay__expandMoreIcon"/>
+                                </div>
+
+                            </div>
+                        </div>
+                        
+                        <button>{searchGuest? "Reserve" : "Check availability"}</button>
+                        {searchGuest? total: ""}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
